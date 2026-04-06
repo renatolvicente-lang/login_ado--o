8d560ded4d93d8f9d06fd90ca78496b34c7ad6b4
renatolvicente-lang <renato_l_vicente@estudante.sesisenai.org.br>
2026-04-06 12:04:15 -0300

    up -  melhorando a  estrutura

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+document.getElementById("form_adc").addEventListener("submit", function(e){
+    e.preventDefault();//predefine um comportamento padrão de formulario.
+
+
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let moradia = document.getElementById("moradia").value;
+    let telefone = document.getElementById("telefone").value;
+    let cpf = document.getElementById("cpf").value;
+    
+    //pegar campo pelo seletor
+    let quintal = document.querySelector('input["quintal"]:checked');
+
+    if(nome.length < 3){//Verifica o tamanho do nome
+        return alert("Nome invalido!!");
+    }
+
+    document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!!<br>" + "Nome: " + nome;// exibe o resultado dentro da div com id de resultado
+
+
+});
