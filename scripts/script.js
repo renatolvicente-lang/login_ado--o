08b98c2df5ba0c11aa5913cd6b53272644c072d1
renatolvicente-lang <renato_l_vicente@estudante.sesisenai.org.br>
2026-04-08 12:53:09 -0300

    feat - arrumando bugs

--- scripts/script.js
+++ scripts/script.js
@@ -9,23 +9,15 @@ document.getElementById("form_adc").addEventListener("submit", function(e){
     let cpf = document.getElementById("cpf").value;
     let idade = document.getElementById("idade").value;
 
-
-
-
-
-
     //pegar campo pelo seletor
-    let quintal = document.querySelector('input["quintal"]:checked');
+    let quintal = document.querySelector('input[name="quintal"]:checked');
+
      
-    if(nome.length < 3){//Verifica o tamanho do nome
-        return alert("Nome invalido!!");
-    }
-    if(cpf.length == 0){
-        return alert("CPF invalido");
-    }
-    if(idade.length < 2 && idade < 18){
-        return alert("Idade invalida");
-    }
+    if(nome.length < 3) return alert("Nome invalido!!");//Verifica o tamanho do nome
+    
+    if(cpf.length == 0) return alert("CPF invalido");
+    
+    if(idade.length < 2 && idade < 18) return alert("Idade invalida");
 
 
     document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!!<br>" + "Nome: " + nome;// exibe o resultado dentro da div com id de resultado
