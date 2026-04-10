8d92af3497942a517df6d6c307d722824da60a0e
renatolvicente-lang <renato_l_vicente@estudante.sesisenai.org.br>
2026-04-10 11:14:35 -0300

    feat - add funcionalidade de verificação de cidade

--- scripts/script.js
+++ scripts/script.js
@@ -8,18 +8,22 @@ document.getElementById("form_adc").addEventListener("submit", function(e){
     let telefone = document.getElementById("telefone").value;
     let cpf = document.getElementById("cpf").value;
     let idade = document.getElementById("idade").value;
-
+    let cidade = document.getElementById("cidade").value;
+      
+    
     //pegar campo pelo seletor
     let quintal = document.querySelector('input[name="quintal"]:checked');
-
+    let pet = document.querySelector('input[name="pet"]:checked');
      
     if(nome.length < 3) return alert("Nome invalido!!");//Verifica o tamanho do nome
     
-    if(cpf.length == 0) return alert("CPF invalido");
+    if(cpf.length == 0) return alert("CPF obrigatorio");
     
     if(idade.length < 2 && idade < 18) return alert("Idade invalida");
 
+    if(cidade.length == 0) return alert("Cidade obrigatoria");
 
+    
     document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!!<br>" + "Nome: " + nome;// exibe o resultado dentro da div com id de resultado
 
 
