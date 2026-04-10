253c571e12212ceb67038677b58432855eac6572
renatolvicente-lang <renato_l_vicente@estudante.sesisenai.org.br>
2026-04-10 11:44:12 -0300

    feat - add verificação de moradia e modificações no js

--- scripts/script.js
+++ scripts/script.js
@@ -9,23 +9,29 @@ document.getElementById("form_adc").addEventListener("submit", function(e){
     let cpf = document.getElementById("cpf").value;
     let idade = document.getElementById("idade").value;
     let cidade = document.getElementById("cidade").value;
-      
-    
+    let tempo = document.getElementById("temp").value;
+    let mtv_adocao = document.getElementById("mtv_adocao").value;
+
     //pegar campo pelo seletor
     let quintal = document.querySelector('input[name="quintal"]:checked');
     let pet = document.querySelector('input[name="pet"]:checked');
      
     if(nome.length < 3) return alert("Nome invalido!!");//Verifica o tamanho do nome
     
-    if(cpf.length == 0) return alert("CPF obrigatorio");
+    if(cpf.length < 11 ) return alert("CPF obrigatório");
     
+    if(telefone.length == 0) return alert("Telefone obrigatório");
+
     if(idade.length < 2 && idade < 18) return alert("Idade invalida");
 
     if(cidade.length == 0) return alert("Cidade obrigatoria");
 
-    if(email.includes('@')) return alert("Email deve conter @")
+    if(!email.includes('@')) return alert("Email deve conter @");// verifica se possui o arroba
+
+    if(moradia == "" || moradia === "0") return alert("O tipo de moradia deve estar selecionado");// verifica se moradia está selecionada
 
     
+
     document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!!<br>" + "Nome: " + nome;// exibe o resultado dentro da div com id de resultado
 
 
